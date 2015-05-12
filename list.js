f30e3d711836c95b7a66112c0c0bfa9a798fe0a3
AsiaLi <254643076@qq.com>
2015-05-12 18:17:10 +0800

    create

---
+++ list.js
@@ -0,0 +1,8 @@
+eg = 'http://create.maka.im/admin/manage';
+swiper = 'http://www.idangero.us/swiper/';
+list = {
+	'arrowing' : 'arrowing.html',
+	'pullDown' : 'pullDown.html',
+	'flipInY' : 'flipInY.html',
+	'zoomIn' : 'zoomIn.html',
+};
